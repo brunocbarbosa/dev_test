b405eba38c4059ec36815eb431347e6a290dfa21
brunocbarbosa <bruno.cbarbosa90@gmail.com>
2024-11-11 22:20:59 -0300

    Revert "configuring dependencies"
    This reverts commit a2e0ffa825d7d49f44140bd9d3f550a398c2d1e8.

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,14 +1,3 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de Post
-@Entity()
-export class Post {
-  @PrimaryGeneratedColumn()
-  title : number
-
-  @Column()
-  description : string
-
-  @Column()
-  userId : string
-}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,17 +1,3 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de User
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number
-
-  @Column()
-  firstName: string
-
-  @Column()
-  lastName: string
-
-  @Column()
-  email: string
-}

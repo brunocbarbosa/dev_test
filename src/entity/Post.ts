2c20f1adec4973138c042413e203bdf120f5d9e2
brunocbarbosa <bruno.cbarbosa90@gmail.com>
2024-11-11 22:26:39 -0300

    configurig dependences

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,14 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  title : number
+
+  @Column()
+  description : string
+
+  @Column()
+  userId : string
+}
